254c97dab4ca267eb6d77cc4aba48d2df4ebfea3
Max <myEmail@example.com>
2021-02-18 21:03:37 +0200

    Added detail page

--- videodown/ytdown/urls.py
+++ videodown/ytdown/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="yt_home"),
-    path('download/', views.yt_download, name='yt_download')
+    path('detail/', views.detail, name='yt_detail'),
+    path('download/', views.yt_download, name='yt_download'),
 ]
--- videodown/ytdown/views.py
+++ videodown/ytdown/views.py
@@ -1,19 +1,24 @@
-from django.shortcuts import render, redirect
-from django.http import FileResponse
 import pytube
+
+from django.shortcuts import render
+from django.http import FileResponse, HttpResponse
 
 
 def index(request):
     return render(request, 'index.html')
 
 
+def detail(request):
+    video_url = request.POST['yt_video_url']
+    global video
+    video = pytube.YouTube(video_url)
+    streams = video.streams.all()
+    # for stream in streams:
+    #     stream.
+    return render(request, 'detail.html', {'video': video, 'streams': streams})
 
 
 def yt_download(request):
-    try:
-        video_url = request.POST['yt_video_url']
-        yt = pytube.YouTube(video_url).streams.first()
-        return FileResponse(open(yt.download(), 'rb'), as_attachment=True)
-    except:
-        return redirect(index)
-
+    stream_itag = request.POST['stream_itag']
+    stream = video.streams.get_by_itag(stream_itag)
+    return FileResponse(open(stream.download(skip_existing=True), 'rb'), as_attachment=True)
